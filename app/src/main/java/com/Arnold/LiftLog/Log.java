28b82c63dfb53dd350426f31ddcb810cf1d28817
mikefeilbach <mikefeilbach@gmail.com>
2015-03-27 20:11:15 -0500

    Log class added. Implementation to follow.

---
+++ app/src/main/java/com/Arnold/LiftLog/Log.java
@@ -0,0 +1,11 @@
+package com.Arnold.LiftLog;
+
+/**
+ * Created by Mike Feilbach on 3/21/2015.
+ * This class describes a Log object. Logs will be held in a Log
+ * Table within the app's database.
+ */
+public class Log
+{
+    //
+}

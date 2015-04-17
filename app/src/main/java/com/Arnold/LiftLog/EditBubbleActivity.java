53c3439bf77f1aeee1446dc17b76c8d0afe7acba
gnygard <gnygard@wisc.edu>
2015-04-17 12:20:39 -0500

    Temporary bubble layout

--- app/src/main/java/com/Arnold/LiftLog/EditBubbleActivity.java
+++ app/src/main/java/com/Arnold/LiftLog/EditBubbleActivity.java
@@ -4,21 +4,29 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.GridLayout;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.Spinner;
 import android.widget.TableRow;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-public class EditBubbleActivity extends ActionBarActivity {
+public class EditBubbleActivity extends ActionBarActivity implements Comparable{
 
     private List<Bubble> bubbles = new ArrayList<>();
 
@@ -27,6 +35,8 @@ public class EditBubbleActivity extends ActionBarActivity {
     final DatabaseHandler db = new DatabaseHandler(this);
 
     private final int max_bubble_length = 20;
+
+    private Spinner bubble_types;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,71 +61,123 @@ public class EditBubbleActivity extends ActionBarActivity {
                 saveBubble();
             }
         });
+
+        bubble_types = (Spinner) findViewById(R.id.type_spinner);
     }
 
     public void updateBubbles() {
 
-        // Initialize the Grid
-        //TableLayout tableLayout = (TableLayout) findViewById(R.id.View_Bubbles); <--Futile attempt
-        //LinearLayout layout = (LinearLayout) findViewById(R.id.View_Bubbles); <-- Somewhat futile
-        GridLayout gridLayout = (GridLayout) findViewById(R.id.View_Bubbles);
+        // Initialize the Layout
+        LinearLayout layout = (LinearLayout) findViewById(R.id.View_Bubbles);
 
-        //tableLayout.removeAllViews();
-        gridLayout.removeAllViews();
+        layout.setHorizontalGravity(Gravity.CENTER_HORIZONTAL);
 
-        gridLayout.setColumnCount(3); // Default, get rid of MAGIC
+        layout.removeAllViews();
 
         this.bubbles = db.getAllBubbles();
 
-        //List<Button> rowBubbles = new ArrayList<>();
-        //List<TableRow> rows = new ArrayList<>();
+        //Collections.sort(bubbles);
 
-        int bubbleListIndex = 0;
         //loop that creates buttons based on the number of logs stored in the database
         //these buttons will be scrollable because of the xml file. Clicking on a button will
         //bring you to another activity in which you can see the contents of the log
 
         // The double while loop was part of an earlier attempt with the TableLayout,
         // disregard the unnecessary inner loop
-        while (bubbleListIndex < bubbles.size()) {
 
-            int widthMax = 300;
-            int currWidth = 0;
+        //sets button parameters
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.MATCH_PARENT);
 
-            while (currWidth < widthMax) {
+        // Scrollview for showing exercise bubbles
+        ScrollView exercise_scroll = new ScrollView(this);
+        LinearLayout exercise_bubs = new LinearLayout(this);
+        exercise_bubs.setOrientation(LinearLayout.VERTICAL);
+        exercise_bubs.setVerticalScrollBarEnabled(true);
+        exercise_scroll.addView(exercise_bubs, lp);
+        //layout.addView(exercise_bubs, lp);
 
-                if (bubbleListIndex == bubbles.size()) break;
-                if ((currWidth + 90) > widthMax) break;
+        // Scrollview for showing repetition bubbles
+        ScrollView reps_sets_scroll = new ScrollView(this);
+        LinearLayout reps_sets_bubs = new LinearLayout(this);
+        reps_sets_bubs.setOrientation(LinearLayout.VERTICAL);
+        reps_sets_bubs.setVerticalScrollBarEnabled(true);
+        reps_sets_scroll.addView(reps_sets_bubs, lp);
+        //layout.addView(reps_sets_bubs, lp);
 
-                //new button being created for log
-                Button myButton = new Button(this);
+        // Scrollview for showing duration bubbles
+        ScrollView duration_scroll = new ScrollView(this);
+        LinearLayout duration_bubs = new LinearLayout(this);
+        duration_bubs.setOrientation(LinearLayout.VERTICAL);
+        duration_bubs.setVerticalScrollBarEnabled(true);
+        duration_scroll.addView(duration_bubs, lp);
+        //layout.addView(duration_bubs, lp);
 
-                //set the text of the log to be the logs title (will add date later)
-                myButton.setText(bubbles.get(bubbleListIndex).getBubbleContent());
-                myButton.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT));
+        //int tester = 0;
 
-                //int bubbleWidth = bubbles.get(bubbleListIndex).getBubbleContent().length();
+        for (Bubble curr_bubble : bubbles) {
 
-                myButton.setClickable(true);
+            //new button being created for bubble
+            Button myButton = new Button(this);
 
-                myButton.setWidth(200);
+            //set the text of the log to be the logs title (will add date later)
+            myButton.setText(curr_bubble.getBubbleContent());
 
-                // Any Click automatically deletes the first bubble, this will be fixed with Lauro's stuff
-                myButton.setOnClickListener(new View.OnClickListener() {
-                    public void onClick(View v) {
-                        db.deleteBubble(bubbles.get(0).getBubbleContent());
-                        updateBubbles();
-                    }
-                });
+            myButton.setClickable(true);
 
-                gridLayout.addView(myButton, currWidth);
-                currWidth++;
-                bubbleListIndex++;
+            myButton.setPadding(2,2,2,2);
+
+            myButton.setMaxWidth(350);
+
+            // Any Click automatically deletes the first bubble, this will be fixed with Lauro's stuff
+            myButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    updateBubbles();
+                }
+            });
+
+
+            if (curr_bubble.getBubbleType() == 0) {
+                myButton.setBackgroundColor(Color.GREEN);
+                exercise_bubs.addView(myButton);
+            } else if (curr_bubble.getBubbleType() == 1) {
+                myButton.setBackgroundColor(Color.RED);
+                reps_sets_bubs.addView(myButton);
+            } else {
+                myButton.setBackgroundColor(Color.BLUE);
+                duration_bubs.addView(myButton);
             }
         }
+//        exercise_scroll.addView(exercise_bubs);
+//        reps_sets_scroll.addView(reps_sets_bubs);
+//        duration_scroll.addView(duration_bubs);
+
+        ScrollView.LayoutParams scroll = new ScrollView.LayoutParams(ScrollView.LayoutParams.WRAP_CONTENT, ScrollView.LayoutParams.MATCH_PARENT);
+
+//        ScrollView.LayoutParams ex_scroll = new ScrollView.LayoutParams(, ScrollView.LayoutParams.MATCH_PARENT);
+//        ScrollView.LayoutParams rep_scroll = new ScrollView.LayoutParams(ScrollView.LayoutParams.WRAP_CONTENT, ScrollView.LayoutParams.MATCH_PARENT);
+//        ScrollView.LayoutParams time_scroll = new ScrollView.LayoutParams(ScrollView.LayoutParams.WRAP_CONTENT, ScrollView.LayoutParams.MATCH_PARENT);
+
+        layout.addView(exercise_scroll, scroll);
+        layout.addView(reps_sets_scroll, scroll);
+        layout.addView(duration_scroll, scroll);
     }
 
     public void saveBubble(){
+
+        String bubble_type_string = String.valueOf(bubble_types.getSelectedItem());
+
+        int bubble_type = -1;
+
+        if (bubble_type_string.equals("Exercise")) {
+            bubble_type = 0;
+        } else if (bubble_type_string.equals("Reps/Sets")) {
+            bubble_type = 1;
+        } else if (bubble_type_string.equals("Weight/Rest")) {
+            bubble_type = 2;
+        } else {
+            this.bubbleContentInput.setError("Invalid Bubble type.");
+            return;
+        }
 
         //Add code to save the bubble
         String bubbleContent = this.bubbleContentInput.getText().toString();
@@ -127,8 +189,7 @@ public class EditBubbleActivity extends ActionBarActivity {
             return;
         }
 
-        // TODO: Add a way for user to pick which type of Bubble is being added.
-        boolean success = db.addBubble(new Bubble(bubbleContent, Bubble.BUBBLE_TYPE_REPS_SETS));
+        boolean success = db.addBubble(new Bubble(bubbleContent, bubble_type));
 
         if (success) {
             Toast.makeText(this, "Yeah, Bubble Saved!", Toast.LENGTH_SHORT).show();
@@ -146,7 +207,6 @@ public class EditBubbleActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.menu_edit_bubble, menu);
         return true;
     }*/
-
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -192,5 +252,10 @@ public class EditBubbleActivity extends ActionBarActivity {
             //If there's no text, just returns to Home Screen
             this.finish();
         }
+    }
+
+    @Override
+    public int compareTo(Object another) {
+        return 0;
     }
 }

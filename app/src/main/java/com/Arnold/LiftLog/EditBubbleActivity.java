3bd471e2066f8b1357f6905ba21c7527e2be0023
zzhengz <zarickzheng@gmail.com>
2015-04-27 17:02:53 -0500

    Edit bubble screen changed
    Add one shape for spinner outfit and one style file for spinner action
    bar

--- app/src/main/java/com/Arnold/LiftLog/EditBubbleActivity.java
+++ app/src/main/java/com/Arnold/LiftLog/EditBubbleActivity.java
@@ -93,6 +93,13 @@ public class EditBubbleActivity extends ActionBarActivity implements Comparable,
         });
 
         bubble_types = (Spinner) findViewById(R.id.type_spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.bubble_types, R.layout.spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        bubble_types.setAdapter(adapter);
     }
 
     public void updateBubbles() {
